db95681f09d8772443006efb0e33c6b222958265
Peilin <peilin_yu@brown.edu>
2020-11-28 23:10:16 -0500

    Update

--- main_lincls.py
+++ main_lincls.py
@@ -106,6 +106,7 @@ def main():
     args.distributed = args.world_size > 1 or args.multiprocessing_distributed
 
     ngpus_per_node = torch.cuda.device_count()
+    print('Using {:d} CUDA Devices!'.format(ngpus_per_node))
     if args.multiprocessing_distributed:
         # Since we have ngpus_per_node processes per node, the total world_size
         # needs to be adjusted accordingly
